b4672df2c34cf39723f0d7df0c9a74a7f5d6b7d7
GedGonz <gedgonz7@gmail.com>
2019-01-29 00:00:05 -0600

    Inital Proyect

---
+++ Controllers/ConsultaController.cs
@@ -0,0 +1,30 @@
+﻿using MVCAJAXA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAJAXA.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        //
+        // GET: /Consulta/
+
+        List<PersonaModel> Persona = new List<PersonaModel>() { new PersonaModel() { Codigo = 1, Nombre = "Gerald", Apellido = "Gonzalez" }, new PersonaModel() { Codigo = 2, Nombre = "Helen", Apellido = "Morales" } };
+       
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult _Consulta(int codigo) 
+        {
+            return PartialView("_Consulta", Persona.Where(x=>x.Codigo==codigo).ToList());
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAJAXA.MvcApplication" Language="C#" %>
